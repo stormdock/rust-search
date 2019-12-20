1be75efb4f606b5c891e9dd05816c44c58abf1f4
Michael Angerman <stormasm@gmail.com>
2019-12-20 11:02:54 -0500

    iterating over the fields

--- tantivy/tan00/examples/schema_fields.rs
+++ tantivy/tan00/examples/schema_fields.rs
@@ -1,11 +1,6 @@
-//use tantivy::collector::TopDocs;
-//use tantivy::query::QueryParser;
-// use tantivy::schema::*;
-//use std::fs::{create_dir, remove_dir_all};
 use std::path::Path;
-//use tantivy::schema::{Document, Schema, STORED, TEXT};
 use tantivy::Index;
-//
+
 fn main() -> tantivy::Result<()> {
     let directory = Path::new("/tmp/tantivy/idxhn");
     let dir_exists = directory.exists();
@@ -15,6 +10,10 @@ fn main() -> tantivy::Result<()> {
 
     let index = Index::open_in_dir(&directory)?;
     let schema = index.schema();
+
+    for (x, y) in schema.fields() {
+        println!("{:?} {:?}", x, y);
+    }
 
     Ok(())
 }
