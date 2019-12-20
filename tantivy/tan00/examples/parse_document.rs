e27aa7f2c10684a58aa38872a7df16021451917d
Michael Angerman <stormasm@gmail.com>
2019-12-20 12:57:02 -0500

    mw

--- tantivy/tan00/examples/parse_document.rs
+++ tantivy/tan00/examples/parse_document.rs
@@ -1,10 +1,9 @@
-use std::path::Path;
 use serde_json::{self, Map as JsonObject, Value as JsonValue};
+use std::path::Path;
 
-use tantivy::Index;
 use tantivy::schema::{DocParsingError, Document};
+use tantivy::Index;
 
-/// Build a document object from a json-object.
 pub fn parse_document(doc_json: &str) -> Result<Document, DocParsingError> {
     let json_obj: JsonObject<String, JsonValue> = serde_json::from_str(doc_json).map_err(|_| {
         let doc_json_sample: String = if doc_json.len() < 20 {
@@ -17,30 +16,7 @@ pub fn parse_document(doc_json: &str) -> Result<Document, DocParsingError> {
 
     let mut doc = Document::default();
     for (field_name, json_value) in json_obj.iter() {
-        /*
-                let field = self
-                    .get_field(field_name)
-                    .ok_or_else(|| DocParsingError::NoSuchFieldInSchema(field_name.clone()))?;
-                let field_entry = self.get_field_entry(field);
-                let field_type = field_entry.field_type();
-
-                match *json_value {
-                    JsonValue::Array(ref json_items) => {
-                        for json_item in json_items {
-                            let value = field_type
-                                .value_from_json(json_item)
-                                .map_err(|e| DocParsingError::ValueError(field_name.clone(), e))?;
-                            doc.add(FieldValue::new(field, value));
-                        }
-                    }
-                    _ => {
-                        let value = field_type
-                            .value_from_json(json_value)
-                            .map_err(|e| DocParsingError::ValueError(field_name.clone(), e))?;
-                        doc.add(FieldValue::new(field, value));
-                    }
-                }
-        */
+        println!("{} {}", field_name, json_value);
     }
     Ok(doc)
 }
@@ -64,7 +40,10 @@ fn main() -> tantivy::Result<()> {
             "id": 123456,
             "score": 4
     }"#;
-    let doc = schema.parse_document(doc_json).unwrap();
+
+    // this works too !
+    // let doc = schema.parse_document(doc_json).unwrap();
+    let doc = parse_document(doc_json).unwrap();
 
     Ok(())
 }
