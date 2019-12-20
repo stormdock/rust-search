4cb40bed1306ccc688387c53336598319d79e95a
Michael Angerman <stormasm@gmail.com>
2019-12-20 06:48:34 -0500

    update the tmp directory of basic_search1

--- tantivy/tan00/examples/basic_search1.rs
+++ tantivy/tan00/examples/basic_search1.rs
@@ -21,13 +21,13 @@ use tantivy::schema::{Document, Schema, STORED, TEXT};
 use tantivy::{doc, Index, ReloadPolicy};
 
 fn main() -> tantivy::Result<()> {
-    let check_path = Path::new("/tmp/tantivy");
+    let check_path = Path::new("/tmp/tantivy/idxbs");
     let dir_exists = check_path.exists();
     if dir_exists {
         remove_dir_all(check_path).expect("dir does not exist");
     }
 
-    let index_path = Path::new("/tmp/tantivy");
+    let index_path = Path::new("/tmp/tantivy/idxbs");
     create_dir(index_path).expect("dir already exists");
 
     // # Defining the schema
